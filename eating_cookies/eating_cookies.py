2dc43435f2a90be60623c0fe648743b623cd8027
Brianna Keune <briannakeune@gmail.com>
2020-06-17 18:22:10 -0500

    plan of eating cookies?

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,11 +4,31 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
+    total_ways = 0
+    if n >= 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
+    if n > 3:
+        total_ways += eating_cookies(n - 3)
+    
+    return total_ways
 
-    pass
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+
+
+'''
+input, number of cookies to be eaten
+output, how many ways he could eat amount of cookies
+
+
+'''
